e6a0ce826c652fd1c61d536cdb33c108c5162b19
Neerajkumar <ny587227@gmail.com>
2022-03-09 21:40:17 +0530

    login page added

--- js/script.js
+++ js/script.js
@@ -36,9 +36,19 @@ document.getElementById("track-train-status").addEventListener("click",()=>{
     trackTrainStatus.style.display="none"
 })
 
+// Robbery page toggling
 robbery.addEventListener("click",()=>{
     document.getElementById("online-complaint").style.display="block"
 })
 document.getElementById("robbery-cancel").addEventListener("click",()=>{
     document.getElementById("online-complaint").style.display="none"
+})
+
+
+// login page toggling
+document.getElementById("login-btn").addEventListener("click",()=>{
+    document.getElementById("login").style.display='block'
+})
+document.getElementById("login-cancel").addEventListener("click",()=>{
+    document.getElementById("login").style.display="none"
 })
